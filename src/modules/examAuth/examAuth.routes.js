8295a649104bdf964f003a08d0f12cf20c1b5e08
Milan Kumar Dandapat <milankumar07@outlook.com>
2026-04-24 17:47:26 +0530

    update for exam mode

--- src/modules/examAuth/examAuth.routes.js
+++ src/modules/examAuth/examAuth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { assignUsersToContest } = require("./examAuth.controller");
 
 // ✅ ONLY ONE IMPORT
 const { loginExam, createExamUser } = require("./examAuth.controller");
