1e826d91a3cb5c5d41df835c8ee0a4161bd4be1b
Milan Kumar Dandapat <milankumar07@outlook.com>
2026-04-25 18:47:38 +0530

    update for exam mode

--- src/modules/examAuth/examAuth.model.js
+++ src/modules/examAuth/examAuth.model.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 
 const examAuthSchema = new mongoose.Schema({
-  userId: { type: String, required: true },      // exam ID (unique)
-  password: { type: String, required: true },      // hashed
+  userId: { type: String, required: true },
+  password: { type: String, required: true },
+
+  plainPassword: {            // 🔥 ADD THIS BLOCK
+    type: String,
+    select: false
+  },
+
   contestId: { 
-  type: mongoose.Schema.Types.ObjectId, 
-  ref: "Contest",
-  required: true 
-},
-  isUsed: { type: Boolean, default: false } // optional (one-time login)
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Contest",
+    required: true 
+  },
+
+  isUsed: { type: Boolean, default: false }
 });
 examAuthSchema.index({ userId: 1, contestId: 1 }, { unique: true });
 module.exports = mongoose.model("ExamAuth", examAuthSchema);
