f9f393cf9d455a80ddeae86b7afe3960e34dee5c
Milan Kumar Dandapat <milankumar07@outlook.com>
2026-04-25 19:31:37 +0530

    update for exam mode

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,4 +1,4 @@
-const { admin } = require("../../config/firebase");
+const admin = require("../../config/firebase");
 const User = require("../user/user.model");
 const jwt = require("jsonwebtoken");
 
